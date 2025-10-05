8ac45f58f77810a3874c16f7000c5b7927322e86
unknown <amanmuhammad567@gmail.com>
2025-10-05 19:20:48 +0500

    Added Some new Features

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,21 +6,21 @@ export const signup = async(req, res)=>{
  const { fullName, email, password } = req.body;
  if(!fullName || !email || !password){
     return res.status(400).json({
-        sucess:false,
+        success:false,
         message:"Please provide all details..."
     })
  }
  const emailRejex= /^\S+@\S+\.\S+$/ ;
  if(!emailRejex.test(email)){
     return res.status(400).json({
-        sucess:false,
+        success:false,
         message:"Invalid Email..."        
     })
  }
  
  if(password.length<8 || password.length>16){
     return res.status(400).json({
-        sucess:false,
+        success:false,
         message:"Password must contain 8 characters and must not contain more than 16 characters..."        
     })
  }
@@ -28,7 +28,7 @@ export const signup = async(req, res)=>{
 
  if(isemailAlreadyExists){
     return res.status(400).json({
-        sucess:false,
+        success:false,
         message:"User with this Email Already Exists..."        
     })
  }
@@ -50,11 +50,49 @@ export const signup = async(req, res)=>{
 }
 
 export const signin = async(req, res)=>{
-    
+    const {email, password} =req.body;
+    if(!email || !password){
+    return res.status(400).json({
+        success:false,
+        message:"Please provide all details..."
+    })
+ }
+    const emailRejex= /^\S+@\S+\.\S+$/ ;
+ if(!emailRejex.test(email)){
+    return res.status(400).json({
+        sucess:false,
+        message:"Invalid Email..."        
+    })
+ }
+
+ let user=await User.findOne({email});
+ if(!user){
+    return res.status(400).json({
+        success:false,
+        message:"No user with this email exists..."
+    })
+ }
+ const isPassword=await bcrypt.compare(password,user.password);
+ if(!isPassword){
+    return res.status(400).json({
+        success:false,
+        message:"Wrong Password Entered..."
+    })
+ }
+ generateToken(user, "Login Successfully", 200, res)
+
 }
 
 export const signout = async(req, res)=>{
-    
+    res.status(200).cookie("usertoken","",{
+        maxAge : 0,
+        httpsOnly : true,
+        sameSite : "strict",
+        secure : process.env.CURR_STATUS !== "development" ? true : false,
+    }).json({
+        success:true,
+        message: "User Logged Out Successfully"
+    })
 }
 
 export const getUser = async(req, res)=>{
