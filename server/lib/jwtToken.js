3a6cabfbcaa78a04da335a36ab6e4cdcd5823587
Muhammad Aman <amanmuhammad567@gmail.com>
2025-11-01 16:14:16 +0500

    Update jwtToken.js

--- server/lib/jwtToken.js
+++ server/lib/jwtToken.js
@@ -1,19 +1,22 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken= async (user, message, statusCode, res)=>{
-    const token=jwt.sign({id: user._id}, process.env.JWT_SECRET,{
-        expiresIn: process.env.JWT_EXPIRE
-    });
+export const generateToken = async (user, message, statusCode, res) => {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
 
-    return res.status(statusCode).cookie("usertoken", token, {
-        httpOnly : true,
-        maxAge : Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000,
-        sameSite : "strict",
-        secure : process.env.CURR_STATUS !== "development" ? true : false,
-    }).json({
-        success : true,
-        user,
-        message,
-        token
+  return res
+    .status(statusCode)
+    .cookie("usertoken", token, {
+      httpOnly: true,
+      maxAge: Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000,
+      sameSite: process.env.CURR_STATUS === "development" ? "lax" : "none", // ✅ FIX
+      secure: process.env.CURR_STATUS !== "development", // ✅ true on Vercel
+    })
+    .json({
+      success: true,
+      user,
+      message,
+      token,
     });
-}
+};
